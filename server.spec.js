bb4e9feccca01906c8dc43bd18839cfcb9c34d82
josh182014 <josh182014@gmail.com>
2019-06-21 09:44:14 -0600

    added two more tests

--- server.spec.js
+++ server.spec.js
@@ -27,13 +27,33 @@ describe('games', () => {
             .send({ title: "New Game"})
             expect(error.status).toBe(422)
         })
+        it('should allow you to post without release year', async () => {
+            const newGame = await supertest(server)
+            .post('/api/games')
+            .send({ title: "Cyberpunk 2077", genre: "open world" })
+            expect(newGame.status).toBe(200)
+        })
     })
 
     describe('GET', () => {
-        it('should return 200', async () => {
+        it('should return list of games in database', async () => {
             const games = await supertest(server)
             .get('/api/games')
             expect(games.status).toBe(200)
+        })
+        it('should return status 200', async () => {
+            await supertest(server)
+            .post('api/games')
+            .send({ title: "Cool Game", genre: "action", realeaseYear: 2019})
+            .send({ title: "Another Game", genre: "adventure", realeaseYear: 1990})
+            const games = await supertest(server)
+            .get('/api/games')
+            expect(games.status).toBe(200)
+        })
+        it('should return indication that there are no games if there are no games stored', async () => {
+            games = await supertest(server)
+            .get('/api/games')
+            expect(games.status).toBe(404)
         })
     })
 })
