589fad9ce4343bf498249ce54811288a80d0182f
josh182014 <josh182014@gmail.com>
2019-06-21 09:38:51 -0600

    wrote server tests

--- server.spec.js
+++ server.spec.js
@@ -6,3 +6,34 @@ afterEach(async () => {
     await db('games').truncate()
 })
 
+// describe('server', () => {
+//     it('should return true', () => {
+//         expect(true).toBe(true)
+//     })
+// })
+
+describe('games', () => {
+    describe('post', () => {
+        it('should post a new game', async () => {
+            const newGame = await supertest(server)
+            .post('/api/games')
+            .send({ title: "Cool Game", genre: "action", realeaseYear: 2019})
+            expect(newGame.status).toBe(200)
+        })
+
+        it("should return 422 if it doesn't receive all needed information", async () => {
+            const error = await supertest(server)
+            .post('/api/games')
+            .send({ title: "New Game"})
+            expect(error.status).toBe(422)
+        })
+    })
+
+    describe('GET', () => {
+        it('should return 200', async () => {
+            const games = await supertest(server)
+            .get('/api/games')
+            expect(games.status).toBe(200)
+        })
+    })
+})
